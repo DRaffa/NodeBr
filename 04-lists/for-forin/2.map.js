a1c2637a438b1fbc7221de0e7e4e2b9aed1943d1
Rafael Jose da Silva <draffa_ti@hotmail.com>
2020-04-19 14:45:12 -0300

    Trabalhando com listas entendendo como funciona o map

--- 04-lists/for-forin/2.map.js
+++ 04-lists/for-forin/2.map.js
@@ -1,18 +1,33 @@
 const service = require('./service')
 
+Array.prototype.meuApp = function (callback) {
+    const novoArrayMapeado = []
+
+    for (let index = 0; index <= this.length - 1; index++) {
+        const resultado = callback(this[index], index)
+        novoArrayMapeado.push(resultado)
+    }
+
+    return novoArrayMapeado
+}
+
 async function main() {
     try {
         const result = await service.obterPessoas('a')
-//        const names = []
-//        result.results.forEach(function (item) {
-//            names.push(item.name)
-//        });
+        //const names = []
+        //result.results.forEach(function (item) {
+        //    names.push(item.name)
+        //});
 
-        const names = result.results.map(function (pessoa){
-            return pessoa.name
+        //const names = result.results.map(function (pessoa){
+        //    return pessoa.name
+        //})
+
+        //const names = result.results.map((pessoa) => pessoa.name)
+
+        const names = result.results.meuApp(function (pessoa, index){
+            return `[${index}-${pessoa.name}]`
         })
-
-        
 
         console.log('nomes', names)
 
