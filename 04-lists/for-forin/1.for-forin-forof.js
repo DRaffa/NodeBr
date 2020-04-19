f38b870a6d24fc46f77a286201c8c35d333b2354
Rafael Jose da Silva <draffa_ti@hotmail.com>
2020-04-19 14:20:23 -0300

    Trabalhando com listas forof

--- 04-lists/for-forin/1.for-forin-forof.js
+++ 04-lists/for-forin/1.for-forin-forof.js
@@ -4,18 +4,25 @@ async function main() {
     try {
         const result = await service.obterPessoas('a')
         const names = []
-//        console.time('for')
-//        for (let index = 0; index < result.results.length; index++) {
-//            const pessoa = result.results[index];
-//            names.push(pessoa.name)
-//        }
-//        console.timeEnd('for')
-        console.time('forin')
-        for (let index in result.results) {
-            const pessoa = result.results[index]
+        //        console.time('for')
+        //        for (let index = 0; index < result.results.length; index++) {
+        //            const pessoa = result.results[index];
+        //            names.push(pessoa.name)
+        //        }
+        //        console.timeEnd('for')
+        //        console.time('forin')
+        //        for (let index in result.results) {
+        //            const pessoa = result.results[index]
+        //            names.push(pessoa.name)
+        //        }
+        //        console.timeEnd('forin')
+
+        console.time('forof')
+        for (const pessoa of result.results) {
             names.push(pessoa.name)
         }
-        console.timeEnd('forin')
+        console.timeEnd('forof')
+
         console.log('nomes', names)
 
     } catch (error) {
